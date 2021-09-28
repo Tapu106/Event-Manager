1a11fa8769520fe1557032b212a11198979536aa
Tapu106 <tapusust993@gmail.com>
2021-09-28 13:27:26 +0600

    styling added

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,30 +1,45 @@
-import Link from "next/link";
+import Button from "../ui/button";
+import classes from "./event-item.module.css";
+import AddressIcon from "../icons/address-icon";
+import DateIcon from "../icons/date-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 function eventItem(props) {
-  const { title, date, address, image, id } = props;
+  const { title, date, location, image, id } = props;
 
   const formattedDate = new Date(date).toLocaleDateString("en-US", {
     day: "numeric",
     month: "long",
     year: "numeric",
   });
-  const fomattedAddress = address.replace(", ", "\n");
+  const fomattedAddress = location.replace(", ", "\n");
   const expolrableLink = `/events/${id}`;
   console.log(image);
   return (
-    <li key={id}>
+    <li key={id} className={classes.item}>
       <img src={"/" + image} alt="" />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h1>{title}</h1>
         </div>
-        <div>
-          <time>{formattedDate}</time>
+        <div className={classes.date}>
+          <time>
+            <DateIcon />
+            {formattedDate}
+          </time>
         </div>
-        <div>
-          <address>{address}</address>
+        <div className={classes.address}>
+          <address>
+            <AddressIcon />
+            {fomattedAddress}
+          </address>
         </div>
-        <div>
-          <Link href={expolrableLink}>Explore Events</Link>
+        <div className={classes.actions}>
+          <Button link={expolrableLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,14 +1,15 @@
 import EventItem from "./event-item";
+import classes from "./event-list.module.css";
 
 function eventLists(props) {
   const { items } = props;
   return (
-    <ul>
+    <ul className={classes.list}>
       {items.map((item) => (
         <EventItem
           key={item.id}
           id={item.id}
-          address={item.location}
+          location={item.location}
           title={item.title}
           image={item.image}
           date={item.date}
