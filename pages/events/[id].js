44b16bf9213f48320a2b46955ae8904e0df33e40
Tapu106 <tapusust993@gmail.com>
2021-09-28 13:53:30 +0600

    event details added

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,9 +1,28 @@
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import { useRouter } from "next/router";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 function eventDetails() {
+  const router = useRouter();
+  const eventId = router.query.id;
+  const eventDetails = getEventById(eventId);
+
+  if (!eventDetails) {
+    return <p>No Event Found!</p>;
+  }
   return (
-    <div>
-      <h1>Title</h1>
-      <h2>Description</h2>
-    </div>
+    <Fragment>
+      <EventSummary title={eventDetails.title} />
+      <EventLogistics
+        date={eventDetails.date}
+        image={eventDetails.image}
+        address={eventDetails.location}
+        imageAlt={eventDetails.title}
+      />
+      <EventContent>{eventDetails.description}</EventContent>
+    </Fragment>
   );
 }
 
